b4357b03507ccb2b79cf516b2c4ad97364f3020a
AndresOrtega1509 <andresf.ortegam@uqvirtual.edu.co>
2026-02-21 17:59:46 -0500

    Se crea el proyecto con las dependencias de SpringBoot

---
+++ app-readNow/app-readNow/src/test/java/co/edu/uniquindio/read_now/AppReadNowApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.read_now;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppReadNowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
