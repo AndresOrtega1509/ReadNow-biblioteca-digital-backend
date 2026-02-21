f376380dd5734b28abdaae8e903c63e3f0404c6b
AndresOrtega1509 <andresf.ortegam@uqvirtual.edu.co>
2026-02-21 17:59:46 -0500

    Se crea el proyecto con las dependencias de SpringBoot

---
+++ app-readNow/app-readNow/src/main/java/co/edu/uniquindio/read_now/AppReadNowApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.read_now;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppReadNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppReadNowApplication.class, args);
+	}
+
+}
