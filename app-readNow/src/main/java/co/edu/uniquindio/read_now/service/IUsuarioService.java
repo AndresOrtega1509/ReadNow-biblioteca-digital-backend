0f0276264aa0c62309078a7ef3ceb07f1944d2f4
AndresOrtega1509 <andresf.ortegam@uqvirtual.edu.co>
2026-03-06 17:35:08 -0500

    Implementación IUsuarioService y UsuarioServiceImpl

---
+++ app-readNow/src/main/java/co/edu/uniquindio/read_now/service/IUsuarioService.java
@@ -0,0 +1,18 @@
+package co.edu.uniquindio.read_now.service;
+
+import co.edu.uniquindio.read_now.dto.request.ActualizarPerfilRequestDTO;
+import co.edu.uniquindio.read_now.dto.request.CambiarPasswordRequestDTO;
+import co.edu.uniquindio.read_now.dto.response.UsuarioResponseDTO;
+
+public interface IUsuarioService {
+
+    UsuarioResponseDTO obtenerPerfil(Long usuarioId);
+
+    UsuarioResponseDTO actualizarPerfil(Long usuarioId, ActualizarPerfilRequestDTO request);
+
+    void cambiarPassword(Long usuarioId, CambiarPasswordRequestDTO request);
+
+    /** Activa o desactiva la verificación en dos pasos para el usuario. */
+    UsuarioResponseDTO actualizarVerificacionDosPasos(Long usuarioId, boolean activo);
+
+}
