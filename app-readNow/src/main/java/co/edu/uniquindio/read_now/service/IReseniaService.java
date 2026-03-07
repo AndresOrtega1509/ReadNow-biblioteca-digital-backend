91245827783959a162da0395dc0910187af55a9e
AndresOrtega1509 <andresf.ortegam@uqvirtual.edu.co>
2026-03-07 03:02:49 -0500

    Implementación IReseniaService y ReseniaServiceImpl

---
+++ app-readNow/src/main/java/co/edu/uniquindio/read_now/service/IReseniaService.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.read_now.service;
+
+import co.edu.uniquindio.read_now.dto.request.ReseniaRequestDTO;
+import co.edu.uniquindio.read_now.dto.response.ReseniaResponseDTO;
+
+import java.util.List;
+
+public interface IReseniaService {
+
+    ReseniaResponseDTO crearResenia(ReseniaRequestDTO request, Long usuarioId);
+
+    List<ReseniaResponseDTO> obtenerReseniasPorRecurso(Long recursoId);
+}
